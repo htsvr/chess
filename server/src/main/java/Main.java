d6fee1b70dbccca0834f4ba1e56a7778b2ecaafb
htsvr <brrgrra@gmail.com>
2025-10-09 10:10:47 -0600

    Implemented GettingStarted for phase 3

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,12 +1,24 @@
 import chess.*;
 
+import server.Server;
+
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
-        var board = new ChessBoard();
-        var pos = new ChessPosition(3, 5);
-        board.addPiece(pos, piece);
-        System.out.println(board);
+        Server server = new Server();
+        server.run(8080);
+
+        System.out.println("♕ 240 Chess Server");
     }
 }
+
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        var piece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
+//        System.out.println("♕ 240 Chess Server: " + piece);
+//        var board = new ChessBoard();
+//        var pos = new ChessPosition(3, 5);
+//        board.addPiece(pos, piece);
+//        System.out.println(board);
+//    }
+//}
