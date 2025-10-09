e61178f34e3947372e7c1f3d077228b5616f7e0e
htsvr <brrgrra@gmail.com>
2025-10-09 10:32:48 -0600

    Added clear and register endpoints

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,24 +1,36 @@
 package server;
 
+import com.google.gson.Gson;
 import io.javalin.*;
+import io.javalin.http.Context;
+
+import java.util.Map;
 
 public class Server {
 
-    private final Javalin javalin;
+    private final Javalin server;
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        server = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
+        server.delete("db", ctx -> ctx.result("{}"));
+        server.post("user", this::register);
+    }
 
+    private void register(Context ctx) {
+        Gson serializer = new Gson();
+        var req = serializer.fromJson(ctx.body(), Map.class);
+        var res = Map.of("username", req.get("username"), "authToken", "12345abc");
+        ctx.result(serializer.toJson(res));
     }
 
     public int run(int desiredPort) {
-        javalin.start(desiredPort);
-        return javalin.port();
+        server.start(desiredPort);
+        return server.port();
     }
 
     public void stop() {
-        javalin.stop();
+        server.stop();
     }
 }
