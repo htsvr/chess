d4681f4bdd12eff20b4bdd5ce8428fd41322e12a
htsvr <brrgrra@gmail.com>
2025-10-14 11:09:54 -0600

    AuthServices added to git

---
+++ server/src/main/java/services/AuthServices.java
@@ -0,0 +1,29 @@
+package services;
+
+import dataaccess.*;
+import dataobjects.*;
+
+import java.util.UUID;
+
+public class AuthServices {
+    private static final AuthDAO authDataAccess = new MemoryAuthDAO();
+
+    public static void clear () {
+        authDataAccess.clear();
+    }
+
+    public static AuthData createAuthToken(String username) {
+        AuthData auth = new AuthData(username, UUID.randomUUID().toString());
+        authDataAccess.createAuth(auth);
+        return auth;
+    }
+
+    public static void logoutUser(String authToken) throws UnrecognizedAuthTokenException, DataAccessException {
+        AuthData auth = authDataAccess.getAuth(authToken);
+        if (auth == null) {
+            throw new UnrecognizedAuthTokenException("Unrecognized Auth Token: " + authToken);
+        } else {
+            authDataAccess.deleteAuth(auth);
+        }
+    }
+}
