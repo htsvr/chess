79bcf4e2057e44036f16aaf90e1bee83fb7bcf2f
htsvr <brrgrra@gmail.com>
2025-10-21 09:41:59 -0600

    Added documentation to methods in GameService

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -14,15 +14,31 @@ public class GameService {
     private static final GameDAO GAME_DATA_ACCESS = new MemoryGameDAO();
     private static final Random RANDOM_GENERATOR = new Random();
 
+    /**
+     * clears everything in the game data database
+     */
     public static void clear() {
         GAME_DATA_ACCESS.clear();
     }
 
+    /**
+     * lists all the games in the database
+     * @param authToken valid auth token
+     * @return a collection all the GameData abjects in the database
+     * @throws UnrecognizedAuthTokenException if the auth token is invalid
+     */
     public static Collection<GameData> listGames(String authToken) throws UnrecognizedAuthTokenException{
         AuthService.validateAuth(authToken);
         return GAME_DATA_ACCESS.getGames();
     }
 
+    /**
+     * creates a game with the given name
+     * @param gameName name of the game to create
+     * @param authToken valid auth token
+     * @return the gameID of the created game
+     * @throws UnrecognizedAuthTokenException if the authToken is invalid.
+     */
     public static int createGame(String gameName, String authToken) throws UnrecognizedAuthTokenException{
         AuthService.validateAuth(authToken);
         int gameID = RANDOM_GENERATOR.nextInt(99998)+1;
@@ -33,6 +49,13 @@ public class GameService {
         return gameID;
     }
 
+    /**
+     * joins a game
+     * @param req JoinRequest object containing the color, gameID, and authToken
+     * @throws UnrecognizedAuthTokenException if the auth token is invalid
+     * @throws AlreadyTakenException if the color specified is already claimed by a user
+     * @throws DataAccessException if the gameID doesn't exist
+     */
     public static void joinGame(JoinRequest req) throws UnrecognizedAuthTokenException, AlreadyTakenException, DataAccessException {
         String username = AuthService.getAuthToken(req.authToken()).username();
         GameData game = GAME_DATA_ACCESS.getGame(req.gameID());
