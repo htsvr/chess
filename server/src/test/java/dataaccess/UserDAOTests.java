748c2d770380cfe56125c9f95b17c2d90a6065ed
htsvr <brrgrra@gmail.com>
2025-10-23 12:30:23 -0600

    Added UnitTests for UserDAO

--- server/src/test/java/dataaccess/UserDAOTests.java
+++ server/src/test/java/dataaccess/UserDAOTests.java
@@ -1,4 +1,82 @@
 package dataaccess;
 
+import dataobjects.UserData;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class UserDAOTests {
+    @ParameterizedTest
+    @ValueSource(classes = {MemoryUserDAO.class})
+    public void getUserSuccess(Class<? extends UserDAO> DAOClass) throws Exception {
+        UserDAO dataAccess = DAOClass.getDeclaredConstructor().newInstance();
+        dataAccess.clear();
+        String username = "exampleName";
+        String email = "example@test.com";
+        String password = "pasword123";
+        UserData user = new UserData(username, password, email);
+
+        String username2 = "newName";
+        String email2 = "newemail@test.com";
+        String password2 = "helloWorld";
+        UserData user2 = new UserData(username2, password2, email2);
+        dataAccess.createUser(user);
+        dataAccess.createUser(user2);
+        assertEquals(user, dataAccess.getUser(username));
+        assertEquals(user2, dataAccess.getUser(username2));
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = {MemoryUserDAO.class})
+    public void getUserFailure(Class<? extends UserDAO> DAOClass) throws Exception {
+        UserDAO dataAccess = DAOClass.getDeclaredConstructor().newInstance();
+        dataAccess.clear();
+        String username = "exampleName";
+        assertThrows(DataAccessException.class, () -> dataAccess.getUser(username));
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = {MemoryUserDAO.class})
+    public void createUserSuccess(Class<? extends UserDAO> DAOClass) throws Exception {
+        UserDAO dataAccess = DAOClass.getDeclaredConstructor().newInstance();
+        dataAccess.clear();
+        String username = "exampleName";
+        String email = "example@test.com";
+        String password = "pasword123";
+        UserData user = new UserData(username, password, email);
+        dataAccess.createUser(user);
+        assertEquals(user, dataAccess.getUser(username));
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = {MemoryUserDAO.class})
+    public void createUserFailure(Class<? extends UserDAO> DAOClass) throws Exception {
+        UserDAO dataAccess = DAOClass.getDeclaredConstructor().newInstance();
+        dataAccess.clear();
+        String username = "exampleName";
+        String email = "example@test.com";
+        String password = "pasword123";
+
+        String email2 = "newemail@test.com";
+        String password2 = "helloWorld";
+        UserData user = new UserData(username, password, email);
+        UserData user2 = new UserData(username, password2, email2);
+        dataAccess.createUser(user);
+        assertThrows(DataAccessException.class, () -> dataAccess.createUser(user2));
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = {MemoryUserDAO.class})
+    public void clearSuccess(Class<? extends UserDAO> DAOClass) throws Exception {
+        UserDAO dataAccess = DAOClass.getDeclaredConstructor().newInstance();
+        dataAccess.clear();
+        String username = "exampleName";
+        String email = "example@test.com";
+        String password = "pasword123";
+        UserData user = new UserData(username, password, email);
+        dataAccess.createUser(user);
+        dataAccess.clear();
+        assertThrows(DataAccessException.class, () -> dataAccess.getUser(username));
+    }
 }
